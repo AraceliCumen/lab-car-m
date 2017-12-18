8892193e338af90f5ceaae96122d9ca75feed582
AraceliCumen <aycuevam@gmail.com>
2017-12-18 12:20:55 -0500

    Añadiendo Primera Sección

---
+++ js/jquery.js
@@ -0,0 +1,20 @@
+/*jQUERY */
+
+/*Modales */
+ $(".button-collapse").sideNav();
+/*modal iniciar sesion*/
+$(document).ready(function(){
+    $('#iniciar-sesion').modal();
+  });
+/*modal registrarse*/
+  $(document).ready(function(){
+      $('#registrate').modal();
+    });
+/*modal conviertet en conductor*/
+$(document).ready(function(){
+    $('#conduce').modal();
+  });
+/*inicializando selectores en el modal conviertete en conductor*/
+  $(document).ready(function() {
+      $('select').material_select();
+    });
